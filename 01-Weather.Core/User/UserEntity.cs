43b85d3fcb26df40307876bf440420748a4a12a0
Ebrahim <ebrahim.rayatparvar@yahoo.com>
2019-05-03 22:39:36 +0430

    02-Create user entity and interface

---
+++ 01-Weather.Core/User/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Weather.Core.User
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string family { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ 02-Weather.Contracts/User/UserRiposetory.cs
@@ -0,0 +1,14 @@
+﻿using _01_Weather.Core.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_Weather.Contracts.User
+{
+    public interface UserRiposetory
+    {
+        void Register(UserEntity obj);
+        void ChanghePassword(UserEntity obj);
+        bool IsUser(UserEntity obj);
+    }
+}
